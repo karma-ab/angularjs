aae5bbdb5bec074ae8c68c70b2b42059dd5cefe3
Abhijit <vishwakarma.abhijit@gmail.com>
2018-05-16 00:46:28 +0530

    callback with http service

--- serviceTest.js
+++ serviceTest.js
@@ -1,19 +1,35 @@
 var app = angular.module("myApp", []);
-app.controller('myController', ['$scope', 'sumService', 'sumFactory', function ($scope, sumService, sumFactory) {
+app.controller('myController', ['$scope', 'sumService', 'sumFactory','$http', function ($scope, sumService, sumFactory,$http) {
 
   $scope.doSum = function () {
-    //  $scope.sum = sumService.doSumService(parseInt($scope.number1),parseInt($scope.number2));
+    sumService.fetchMayors("showMayors",function(resp){
+      $scope.mayors = resp;
+     });
     sumFactory.doSumFactory(parseInt($scope.number1), parseInt($scope.number2), function (result) {
       $scope.sum = result;
     });
   };
 }]);
 
-app.service('sumService', function () {
+app.service('sumService', ['$http','$log',function ($http,$log) {
+  var urlDev = "http://localhost:8484/spring-boot-dev/";
+  var urlProd = "http://localhost:8585/spring-boot-prod/";
+
   this.doSumService = function (num1, num2) {
     return num1 + num2;
-  }
-});
+  };
+
+  this.fetchMayors = function(endpoint,cb){
+    $http({
+      'url':urlDev+endpoint,
+      'method':'GET'
+    }).then(function(resp){
+      cb(resp.data);
+    },function(resp){
+      
+    });
+  };
+}]);
 
 //Normal Sync Method
 /* app.factory('sumFactory', ['$log', function ($log) {
